08b8cac093de87f85c926ce5300a8d0a144adc50
Jaime Soto <xxjacques1234@gmail.com>
2023-12-07 09:42:33 -0400

    arreglo

--- DB/conexion.js
+++ DB/conexion.js
@@ -1,12 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-const dbConexion = new Sequelize( 
-    process.env.PRODUCTION_DB || 'db_cba', 
-    'postgres', 
-    process.env.PRODUCTION_PASSWORD || '12345', {
-        host: process.env.PRODUCTION_HOST || 'localhost',
+const dbConexion = new Sequelize( process.env.PRODUCTION_DB, 'postgres', process.env.PRODUCTION_PASSWORD, {
+        host: process.env.PRODUCTION_HOST,
         dialect: 'postgres'
     });
+// const dbConexion = new Sequelize( 'db_cba', 'postgres', '12345', {
+//         host : 'localhost',
+//         dialect: 'postgres'
+// });
 
 module.exports = {
     dbConexion
