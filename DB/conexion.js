fd1cf8e997fdd60e5b078ae3ab34dac49a5a2c9f
Jaime Soto <xxjacques1234@gmail.com>
2023-12-07 09:51:24 -0400

    Accedo a DB Remoto

--- DB/conexion.js
+++ DB/conexion.js
@@ -1,9 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const dbConexion = new Sequelize( process.env.PRODUCTION_DB, 'postgres', process.env.PRODUCTION_PASSWORD, {
-        host: process.env.PRODUCTION_HOST,
-        dialect: 'postgres'
-    });
+
+const dbConexion = new Sequelize( process.env.PRODUCTION_URL );
 // const dbConexion = new Sequelize( 'db_cba', 'postgres', '12345', {
 //         host : 'localhost',
 //         dialect: 'postgres'
